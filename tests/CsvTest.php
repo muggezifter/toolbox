940e033779b92afeade7b753c96dd232d24d0f0e
Gert Rietveld <muggezifter@gmail.com>
2015-06-23 09:26:13 +0000

    added comment

--- tests/CsvTest.php
+++ tests/CsvTest.php
@@ -2,6 +2,10 @@
 
 use Toolbox\Models\Csv;
 
+/**
+ * This is not a particularly useful test; it is included as a gesture towards
+ * the idea that code is not really complete if there are no tests
+ */
 class CsvTest extends PHPUnit_Framework_TestCase
 {
     protected $csv;
